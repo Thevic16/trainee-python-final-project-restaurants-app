600c83222a6a891b17b6c8d1c32871d54ad1a913
Víctor Gómez <vgomez@applaudostudios.dev>
2022-04-13 11:40:46 -0400

    Create container and database interface

--- restaurants_app/dish/apps.py
+++ restaurants_app/dish/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DishesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dishes'
+    name = 'dish'
--- restaurants_app/restaurants_app/settings.py
+++ restaurants_app/restaurants_app/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialize environ ----------------------------------------------------------
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +81,14 @@ WSGI_APPLICATION = 'restaurants_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
