6b3a84dc550c8a315c14c85d269fd06bc45eea81
Víctor Gómez <vgomez@applaudostudios.dev>
2022-04-18 19:46:11 -0400

    Add Person, Role and DeliveryType

--- restaurants_app/restaurant/models.py
+++ restaurants_app/restaurant/models.py
@@ -9,9 +9,17 @@ class Restaurant(models.Model):
         return self.name
 
 
+class DeliveryType(models.Model):
+    type = models.CharField(max_length=120, unique=True)
+
+    def __str__(self):
+        return self.type
+
+
 class Branch(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     direction = models.TextField(blank=True)
+    delivery_type = models.ManyToManyField(DeliveryType)
 
     def __str__(self):
         return f'branch (id={self.id}) - {self.restaurant.name}'
