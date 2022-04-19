2df5d587140712fe688a62fd722de077e50fcccf
Víctor Gómez <vgomez@applaudostudios.dev>
2022-04-19 14:34:48 -0400

    Add swagger documentation in the docs/ resource

--- restaurants_app/restaurants_app/urls.py
+++ restaurants_app/restaurants_app/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Restaurant API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +26,7 @@ urlpatterns = [
 
     # Inventory Api
     path('api/', include('inventory.api.urls')),
+
+    # Swagger Documentation
+    path('docs/', schema_view),
 ]
